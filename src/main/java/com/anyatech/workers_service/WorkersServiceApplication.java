2a66368f5adae754ad8fddc16ebe5e5e71b127e3
Anmar Hammadi <anmar.hammadi@outlook.com>
2024-05-31 15:43:35 +0100

    initial commit of blank service

---
+++ src/main/java/com/anyatech/workers_service/WorkersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.anyatech.workers_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkersServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkersServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anyatech/workers_service/WorkersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anyatech.workers_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkersServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
