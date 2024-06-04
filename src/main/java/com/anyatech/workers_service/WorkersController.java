d07296815588950aea07479d4f783e29889fd7f7
Anmar Hammadi <anmar.hammadi@outlook.com>
2024-06-04 21:53:15 +0100

    added concrete class for use as a controller to support interface controller generated from openapi

---
+++ src/main/java/com/anyatech/workers_service/WorkersController.java
@@ -0,0 +1,10 @@
+package com.anyatech.workers_service;
+
+import api.PolicyApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WorkersController implements PolicyApi {
+
+    String temp = "testing";
+}
--- src/main/java/com/anyatech/workers_service/WorkersServiceApplication.java
+++ src/main/java/com/anyatech/workers_service/WorkersServiceApplication.java
@@ -2,8 +2,9 @@ package com.anyatech.workers_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class WorkersServiceApplication {
 
     public static void main(String[] args) {
