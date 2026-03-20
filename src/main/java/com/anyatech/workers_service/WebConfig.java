aabe9d91fb1c67f59c98c219a52bbff8fcba5568
Anmar Hammadi <anmar.hammadi@outlook.com>
2026-03-20 00:41:52 +0000

    updated api to represent workers and updated workers controller to implement that api

--- src/main/java/com/anyatech/workers_service/WebConfig.java
+++ src/main/java/com/anyatech/workers_service/WebConfig.java
@@ -1,4 +1,4 @@
-package com.anyatech.worker_service;
+package com.anyatech.workers_service;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,16 +7,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://your-angular-app.amplifyapp.com")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
-            }
-        };
-    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://your-angular-app.amplifyapp.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
